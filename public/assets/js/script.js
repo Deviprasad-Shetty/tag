921a0036dc791d40d8710194944150f20ba697ee
Deviprasad-Shetty <shettydeviprasad1994@gmail.com>
2026-01-05 08:41:37 +0000

    "URL"

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -27,7 +27,7 @@ function addToCart(bookId) {
 // Track page views and events
 const trackEvent = async (eventName, data = {}) => {
   try {
-    const response = await fetch("https://trackingservice-401966967468.us-east1.run.app", {
+    const response = await fetch("https://trackingservice-47621202522.us-west1.run.app", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
